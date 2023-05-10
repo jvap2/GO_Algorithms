711b3dcb4619f9e900a9cd00362d99780c4b86cc
jvap2 <jayvap6181@gmail.com>
2023-05-10 14:18:38 -0600

    Building LinearOperators

--- LinearOperators/linearoperator.go
+++ LinearOperators/linearoperator.go
@@ -6,3 +6,31 @@ type Toeplitz struct{
 	row []float64
 	col []float64
 }
+
+type Tridiagonal struct{
+	size int
+	l_diag []float64
+	diag []float64
+	u_diag []float64
+}
+
+type Vandermonde struct{
+	size int
+	col_1 []float64
+}
+
+type Identity struct{
+	size int
+}
+
+type Diagonal struct{
+	size int
+	diag []float64
+}
+
+type Sparse_CSR struct{
+	NNZ int
+	V []float64
+	COL_INDEX []int64
+	ROW_INDEX []int64
+}
